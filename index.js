c152114d0e7f42292004c8c6f8ac423a91199b02
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-19 19:36:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes={}
 
-Function updateObjectWithKeyAndValue= (recipes,spice,amount){ 
+function updateObjectWithKeyAndValue (recipes,spice,amount){ 
   return recipe.assign({}, recipe, { [spice]: amount })
 }
 
