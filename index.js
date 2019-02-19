f7747c2f882edfdf33f414b023d072be11bd5e97
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-19 19:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue (recipes,spice,amount){
 
 
 function destructivelyUpdateObjectWithKeyAndValue (recipes,spice,amount){ 
-  recipes[spice]="pinch"
+  recipes[spice]=amount
   return recipes
 }
